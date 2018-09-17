ce6360aae5028bbd2b542a705fdc1f3bc84dbe27
Matt <over0219@umn.edu>
2018-09-17 17:09:39 -0500

    added setup scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p dev/MinGfx/build
+cd dev/MinGfx/build
+
+if [ -z "$1" ]
+then
+	cmake .. -DCMAKE_INSTALL_PREFIX=../../.. -DAUTOBUILD_NANOGUI=ON
+else
+	cmake .. -G "$1" -DCMAKE_INSTALL_PREFIX=../../.. -DAUTOBUILD_NANOGUI=ON
+fi
+cmake --build . --target install
